ddbc98ccc8dbe13757f4b16779bbff0eda636c2f
dree <andremongkareng013@gmail.com>
2022-12-13 12:33:09 +0800

    upload gambar for backend traveloke

--- controller/TempatWisataManado.js
+++ controller/TempatWisataManado.js
@@ -1,11 +1,13 @@
 import TempatWisataManado from '../Model/TempatWisataManadoModel.js';
+import path from "path";
+import fs from "fs";
+
 
 export const getTempatWisataManado = async (req, res) => {
+    //SELECT * FROM TempatWisataManado
     try {
-        //SELECT * FROM TempatWisataManado
         const response = await TempatWisataManado.findAll();
         res.status(200).json(response);
-
     } catch (error) {
         console.log(error.message);
     }
@@ -26,15 +28,38 @@ export const getTempatWisataManadoById = async (req, res) => {
 }
 
 export const createTempatWisataManado = async (req, res) => {
-    try {
-        // INSERT
-        await TempatWisataManado.create(req.body);
-        res.status(201).json({
-            msg: 'Tempat Wisata Created'
-        })
-    } catch (error) {
-        console.log(error.messagge);
-    }
+     // INSERT INTO tempatwisata VALUES
+     if (req.files === null || req.files === undefined)
+        return res.status(400).json({ msg: "Gambar belum diupload" });
+    const { nama, alamat, deskripsi, jarak } = req.body;
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    const fileName = file.md5 + ext;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    const allowedType = [".png", ".jpg", ".jpeg"];
+
+    if (!allowedType.includes(ext.toLowerCase()))
+        return res.status(422).json({ msg: "Invalid Images" });
+    if (fileSize > 5000000)
+        return res.status(422).json({msg:"Image must be less than 5 MB"});
+
+    file.mv(`./public/images/${fileName}`, async (err) => {
+        if (err) return res.status(500).json({msg: err.message});
+        try{
+            await TempatWisataManado.create({
+                nama: nama,
+                alamat: alamat,
+                deskripsi:deskripsi,
+                jarak: jarak,
+                image: fileName,
+                url: url,
+            });
+            res.status(201).json({msg: "Product Created Successfuly"})
+        } catch (error) {
+            console.log(error.message);
+        }
+    })
 }
 
 
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import FileUpload from "express-fileupload";
 import TempatWisataManadoRoute from './routes/TempatWisataManadoRoute.js'
-
+import db from "./Config/Database.js"
 const app = express();
+
+(async () => {
+    await db.sync();
+})();
 app.use(cors());
 app.use(express.json());
+app.use(FileUpload());
+app.use(express.static("public"));
 app.use(TempatWisataManadoRoute);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5001, () => console.log('Server running on port 5001'));
